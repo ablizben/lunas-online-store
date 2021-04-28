a2d88fc96a8b3742d3313a86db95632b81989100
Abby Benjamin <ablizben@gmail.com>
2021-04-28 11:25:30 -0400

    editing header and adding dropdown menu for shop

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,10 @@ import { NavLink, BrowserRouter as Router } from 'react-router-dom';
 
 
 const NavBar = () => {
+    const dropDown = () => {
+        document.getElementById("myDropdown").classList.toggle("show");
+}
+    
 
     return (
         <div className="NavBar">
@@ -10,14 +14,21 @@ const NavBar = () => {
             <div className="nav-image">Luna's</div>
             <div className="nav-mid-links">
               <NavLink to='/'>Home</NavLink>
-              <NavLink to='/shop'>Shop</NavLink>
+              <NavLink to='/shop' onClick={dropDown} class='shop'>Shop</NavLink>
+              <div id="myDropdown" class="dropdown-content">
+                  <a href="#">New</a>
+                  <a href="#">Clothing</a>
+                  <a href="#">Accessories</a>
+                  <a href="#">Shoes</a>
+                  <a href="#">Sales</a>
+              </div>
               <NavLink to='/blog'>Blog</NavLink>
               <NavLink to='/support'>Support</NavLink>
             </div>
             <div className="nav-right-links">
             <NavLink to='/registersignin'>Register/Sign in</NavLink>
-              <NavLink to='/search'>Search</NavLink>
-              <NavLink to='/shoppingcart'>Shopping Cart</NavLink>
+              <NavLink to='/search'><i class="fa fa-search"></i></NavLink>
+              <NavLink to='/shoppingcart'><i class="fa fa-shopping-cart"></i></NavLink>
             </div>
             </Router>
         </div>
@@ -25,3 +36,4 @@ const NavBar = () => {
     }
     
     export default NavBar;
+    
