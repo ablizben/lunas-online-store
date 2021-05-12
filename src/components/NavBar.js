32620103281d80b1770e83320089186739de1da0
Abby Benjamin <ablizben@gmail.com>
2021-05-12 16:55:29 -0400

    adding new page for New section

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { NavLink, BrowserRouter as Router } from 'react-router-dom';
 
 
---
+++ src/components/New.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+
+const New = () => {
+   
+
+    return (
+        <div className="New">
+            <div className="new-header">
+                Check out what's new!
+            </div>
+        </div>
+     
+    
+    );
+    }
+    
+    export default New;
+    
